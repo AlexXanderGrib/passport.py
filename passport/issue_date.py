9b5ac39f2c421237867c445aaa12b54d2924c24e
Alexander Gribochkin <me@alexxgrib.me>
2023-07-08 20:00:38 +0300

    feat: Better issue date estimation

--- passport/issue_date.py
+++ passport/issue_date.py
@@ -1,5 +1,10 @@
+from datetime import datetime, timedelta
+
+
 def get_date_of_issue(serial: str, birth_date: str) -> str:
     year_of_issue = serial[2:4]
-    date_of_issue = f'{birth_date[0:5]}.20{year_of_issue}'
+    date_of_issue = f'{birth_date[0:5]}.{year_of_issue}'
 
-    return date_of_issue
+    date = datetime.strptime(date_of_issue, "%d.%m.%y") + timedelta(days=20)
+
+    return date.strftime("%d.%m.%Y")
